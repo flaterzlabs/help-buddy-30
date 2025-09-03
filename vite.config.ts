571c48b554b1632302730fe99a4c858ba58e1912
flaterzlabs <flaterzlabs@gmail.com>
2025-09-03 12:11:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/help-buddy-30",
   server: {
     host: "::",
     port: 8080,
